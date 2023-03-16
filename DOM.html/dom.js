1ed48e8ba082ae2d3e39b1934e69e6da68545a9d
lhulagutte <lhulagutte@gmail.com>
2023-03-16 07:46:13 +0530

    pleaseCommit

--- DOM.html/dom.js
+++ DOM.html/dom.js
@@ -1,16 +1,17 @@
 console.log(document.domain);
 console.log(document.URL);
-console.log(document.title);
-//document.title="123";
-//console.log(document.title);
-console.log(document.head);
-console.log(document.all);
-console.log(document.getElementById('hello'));
+
+//console.log(document.getElementById('hello'));
 var header=document.getElementById('main-header');
 header.style.borderBottom='solid 3px #000';
-var items=document.getElementByClass('list-group-item');
+var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 items[1].textContent='Hello';
 items[1].style.color="green";
 items[1].style.fontWeight="bold";
+items[2].style.backgroundColor="green";
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight="bold";
+}
