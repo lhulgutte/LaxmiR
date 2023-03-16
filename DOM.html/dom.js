602f8737823d11db74f92855e21a0cf119852ce9
lhulagutte <lhulagutte@gmail.com>
2023-03-16 08:10:16 +0530

    GetElementsBtTag

--- DOM.html/dom.js
+++ DOM.html/dom.js
@@ -1,17 +1,23 @@
 console.log(document.domain);
 console.log(document.URL);
 
-//console.log(document.getElementById('hello'));
+console.log(document.getElementById('hello'));
 var header=document.getElementById('main-header');
 header.style.borderBottom='solid 3px #000';
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello';
-items[1].style.color="green";
-items[1].style.fontWeight="bold";
-items[2].style.backgroundColor="green";
-for(var i=0;i<items.length;i++)
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent='Hello';
+//items[1].style.color="green";
+//items[1].style.fontWeight="bold";
+//items[2].style.backgroundColor="green";
+/*for(var i=0;i<items.length;i++)
 {
-    items[i].style.fontWeight="bold";
+   items[i].style.fontWeight="bold";
+}*/
+var li=document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++)
+{
+li[i].style.fontWeight="bold";
 }
+
