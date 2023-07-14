d1e4d3a6b93d55644ca1e80feb7c83ed65ff466b
lhulagutte <lhulagutte@gmail.com>
2023-07-14 17:10:34 +0530

    LocalStorage

---
+++ Localstorage.js
@@ -0,0 +1,24 @@
+function test(){
+var name=document.getElementById("name1").value;
+var email=document.getElementById("email1").value;
+
+//storing data
+var userName=localStorage.setItem("username",name);
+var userEmail=localStorage.setItem("useremail",email);
+
+//retrieving data
+var userName=localStorage.getItem("username",name);
+var userEmail=localStorage.getItem("useremail",email);
+
+var a,b;
+a="xyz";
+b="abc@gmail.com";
+
+if(a==userName && b==userEmail){
+    alert("Successful");
+}
+else{
+    alert("Unsuccessful");
+}
+}
+
--- javcode.js
+++ javcode.js
@@ -1,3 +1,21 @@
-const string="A";
-const result =string.charCodeAt(0);
-console.log(result)
+function main() {
+    var num = 6;
+    console.log(checkNumber(num));
+}
+function checkNumber(num) {
+  // Your code goes here
+  // check if number is greater than 0
+if (num > 0) {
+   return 'positive';
+}
+
+// check if number is 0
+else if (num == 0) {
+ return 'zero';
+}
+
+// if number is less than 0
+else {
+    return 'negative';
+}
+}
